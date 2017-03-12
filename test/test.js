c5ce2551927a800eef669cb84f60361ef12bff07
TomasR <Rakusan2@gmail.com>
2017-03-11 21:07:22 -0500

    Test can throw error

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ const _1 = require("../");
 const ntClient = new _1.Client();
 ntClient.start((con, err) => {
     console.log({ con, err });
+    if (!con)
+        throw err;
 });
 ntClient.addListener((key, val, type, id) => {
     console.log({ key, val, type, id });
--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,7 @@ import { Client } from '../'
 const ntClient = new Client()
 ntClient.start((con, err) => {
     console.log({ con, err })
+    if(!con)throw err
 })
 ntClient.addListener((key, val, type, id) => {
     console.log({ key, val, type, id })
